0cdc6729c4eb446ac32ef1719273062fe16d0296
root <root@>
2017-05-28 15:57:08 +0000

    Dependency libraries are beeing built from sources now

--- build_dep.sh
+++ build_dep.sh
@@ -20,14 +20,84 @@ echo "Building GCC infrastructure"
 IFS_DIR="gcc-infrastructure"
 mkdir ${IFS_DIR}
 cd ${IFS_DIR}
-apt-get install gcc g++ make zlib1g-dev libgmp-dev libmpfr-dev libmpc-dev
-echo "--> libisl"
-if [ ! -f isl-${ISL_VER}.tar.bz2 ]; then
-$WGET_CMD ftp://gcc.gnu.org/pub/gcc/infrastructure/isl-${ISL_VER}.tar.bz2
+apt-get install gcc g++ make zlib1g-dev m4
+
+
+### libgmp
+LIB_NAME="gmp"
+LIB_VER=$GMP_VER
+LIB_ARC="bz2"
+echo "--> lib${LIB_NAME}"
+if [ ! -f ${LIB_NAME}-${LIB_VER}.tar.${LIB_ARC} ]; then
+    $WGET_CMD ftp://gcc.gnu.org/pub/gcc/infrastructure/${LIB_NAME}-${LIB_VER}.tar.${LIB_ARC}
 fi
-rm -rf isl-${ISL_VER}
-tar -xf isl-${ISL_VER}.tar.bz2
-cd isl-${ISL_VER}
-./configure --prefix=/usr
-make -j${NUM_THREADS} install
-cd ..
+rm -rf ${LIB_NAME}-${LIB_VER}
+if [ ! -f /usr/lib/lib${LIB_NAME}.so ]; then
+    tar -xf ${LIB_NAME}-${LIB_VER}.tar.${LIB_ARC}
+    cd ${LIB_NAME}-${LIB_VER}
+    ./configure --prefix=/usr
+    make -j${NUM_THREADS} install
+    cd ..
+else
+    echo "Already installed"
+fi
+
+### libmpfr
+LIB_NAME="mpfr"
+LIB_VER=$MPFR_VER
+LIB_ARC="bz2"
+echo "--> lib${LIB_NAME}"
+if [ ! -f ${LIB_NAME}-${LIB_VER}.tar.${LIB_ARC} ]; then
+    $WGET_CMD ftp://gcc.gnu.org/pub/gcc/infrastructure/${LIB_NAME}-${LIB_VER}.tar.${LIB_ARC}
+fi
+rm -rf ${LIB_NAME}-${LIB_VER}
+if [ ! -f /usr/lib/lib${LIB_NAME}.so ]; then
+    tar -xf ${LIB_NAME}-${LIB_VER}.tar.${LIB_ARC}
+    cd ${LIB_NAME}-${LIB_VER}
+    ./configure --prefix=/usr
+    make -j${NUM_THREADS} install
+    cd ..
+else
+    echo "Already installed"
+fi
+
+
+### libmpc
+LIB_NAME="mpc"
+LIB_VER=$MPC_VER
+LIB_ARC="gz"
+echo "--> lib${LIB_NAME}"
+if [ ! -f ${LIB_NAME}-${LIB_VER}.tar.${LIB_ARC} ]; then
+    $WGET_CMD ftp://gcc.gnu.org/pub/gcc/infrastructure/${LIB_NAME}-${LIB_VER}.tar.${LIB_ARC}
+fi
+rm -rf ${LIB_NAME}-${LIB_VER}
+if [ ! -f /usr/lib/lib${LIB_NAME}.so ]; then
+    tar -xf ${LIB_NAME}-${LIB_VER}.tar.${LIB_ARC}
+    cd ${LIB_NAME}-${LIB_VER}
+    ./configure --prefix=/usr
+    make -j${NUM_THREADS} install
+    cd ..
+else
+    echo "Already installed"
+fi
+
+
+### libisl
+LIB_NAME="isl"
+LIB_VER=$ISL_VER
+LIB_ARC="bz2"
+echo "--> lib${LIB_NAME}"
+if [ ! -f ${LIB_NAME}-${LIB_VER}.tar.${LIB_ARC} ]; then
+    $WGET_CMD ftp://gcc.gnu.org/pub/gcc/infrastructure/${LIB_NAME}-${LIB_VER}.tar.${LIB_ARC}
+fi
+rm -rf ${LIB_NAME}-${LIB_VER}
+if [ ! -f /usr/lib/lib${LIB_NAME}.so ]; then
+    tar -xf ${LIB_NAME}-${LIB_VER}.tar.${LIB_ARC}
+    cd ${LIB_NAME}-${LIB_VER}
+    ./configure --prefix=/usr
+    make -j${NUM_THREADS} install
+    cd ..
+else
+    echo "Already installed"
+fi
+
